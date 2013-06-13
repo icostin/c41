0da59b3a64358de417068e62fffad37cf746da89
Costin Ionescu <costin.ionescu@gmail.com>
2013-06-13 07:36:56 +0100

    dlist: minor tweaks

--- include/c41/dlist.h
+++ include/c41/dlist.h
@@ -21,13 +21,26 @@ union c41_np_u
   c41_np_t * (links[2]);
 };
 
-static __inline c41_np_t * c41_dlist_init (c41_np_t * list_p)
+/* c41_dlist_init ***********************************************************/
+C41_INLINE c41_np_t * c41_dlist_init (c41_np_t * list_p)
 {
   list_p->next = list_p->prev = list_p;
   return list_p;
 }
 
-static __inline void c41_dlist_ins (c41_np_t * anchor_p, c41_np_t * to_ins_p, int dir)
+/* c41_dlist_is_empty *******************************************************/
+C41_INLINE int c41_dlist_is_empty (c41_np_t * list_p)
+{
+  return list_p->next == list_p;
+}
+
+/* c41_dlist_ins ************************************************************/
+C41_INLINE void c41_dlist_ins 
+(
+  c41_np_t * anchor_p, 
+  c41_np_t * to_ins_p, 
+  int dir
+)
 {
   c41_np_t * n;
   to_ins_p->links[dir] = n = anchor_p->links[dir];
@@ -36,9 +49,11 @@ static __inline void c41_dlist_ins (c41_np_t * anchor_p, c41_np_t * to_ins_p, in
   anchor_p->links[dir] = to_ins_p;
 }
 
+/* C41_DLIST_APPEND *********************************************************/
 #define C41_DLIST_APPEND(_list, _obj_p, _np_field) \
   (c41_dlist_ins(&(_list), &(_obj_p)->_np_field, C41_PREV))
 
+/* C41_DLIST_PREPEND ********************************************************/
 #define C41_DLIST_PREPEND(_list, _obj_p, _np_field) \
   (c41_dlist_ins(&(_list), &(_obj_p)->_np_field, C41_NEXT))
 
